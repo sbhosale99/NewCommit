7f5800e1d6334b1d6f95567cfee6a9bac8187ea0
sourabh <sourabhbhosale21@gmail.com>
2025-04-01 14:10:34 +0530

    gheun Tak

--- src/test/NewAddede.java
+++ src/test/NewAddede.java
@@ -9,5 +9,17 @@ System.out.println("Git Added to the branch");
 System.out.println("This from person A");
 
 	}
-
+	
+	public void jUstforShow()
+	{
+		System.out.println("Ye re baba");
+	}
+	public void jUstforShow2()
+	{
+		System.out.println("Ye re baba2");
+	}
+	public void jUstforShow3()
+	{
+		System.out.println("Ye re baba3");
+	}
 }
